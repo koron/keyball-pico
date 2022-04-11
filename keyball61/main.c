4a07f1bde99826830b32bb585c71d5230ade00af
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-04-11 10:27:59 +0900

    communicate with PMW3360

--- keyball61/main.c
+++ keyball61/main.c
@@ -15,25 +15,31 @@ void matrix_suppressed(uint64_t when, uint knum, bool on, uint64_t last, uint64_
 }
 #endif
 
+#define SPI_PORT    spi_default
+#define SPI_RX      PICO_DEFAULT_SPI_RX_PIN
+#define SPI_SCK     PICO_DEFAULT_SPI_SCK_PIN
+#define SPI_TX      PICO_DEFAULT_SPI_TX_PIN
+#define SPI_CSN     PICO_DEFAULT_SPI_CSN_PIN
+
 static inline void cs_select() {
     asm volatile("nop \n nop \n nop");
-    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, false);
+    gpio_put(SPI_CSN, false);
     asm volatile("nop \n nop \n nop");
 }
 
 static inline void cs_deselect() {
     asm volatile("nop \n nop \n nop");
-    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, true);
+    gpio_put(SPI_CSN, true);
     asm volatile("nop \n nop \n nop");
 }
 
 uint8_t reg_read(uint8_t addr) {
     cs_select();
     addr &= 0x7f;
-    spi_write_blocking(PICO_DEFAULT_SPI, &addr, 1);
+    spi_write_blocking(SPI_PORT, &addr, 1);
     sleep_us(160);
     uint8_t data = 0;
-    spi_read_blocking(PICO_DEFAULT_SPI, 0, &data, 1);
+    spi_read_blocking(SPI_PORT, 0, &data, 1);
     sleep_us(20);
     cs_deselect();
     return data;
@@ -42,7 +48,7 @@ uint8_t reg_read(uint8_t addr) {
 void reg_write(uint8_t addr, uint8_t data) {
     uint8_t buf[] = {addr | 0x80, data};
     cs_select();
-    spi_write_blocking(PICO_DEFAULT_SPI, buf, 2);
+    spi_write_blocking(SPI_PORT, buf, 2);
     cs_deselect();
     sleep_us(180);
 }
@@ -50,28 +56,20 @@ void reg_write(uint8_t addr, uint8_t data) {
 uint8_t pid = 0, rev = 0;
 
 void pmw3360_init() {
-#if 1
-    gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
-    bi_decl(bi_3pins_with_func(
-                PICO_DEFAULT_SPI_RX_PIN,
-                PICO_DEFAULT_SPI_TX_PIN,
-                PICO_DEFAULT_SPI_SCK_PIN,
-                GPIO_FUNC_SPI));
+    // SPI bus
+    gpio_set_function(SPI_RX, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_TX, GPIO_FUNC_SPI);
+    bi_decl(bi_3pins_with_func(SPI_RX, SPI_TX, SPI_SCK, GPIO_FUNC_SPI));
+    // SPI CSN
+    gpio_init(SPI_CSN);
+    gpio_set_dir(SPI_CSN, GPIO_OUT);
+    gpio_put(SPI_CSN, true);
+    bi_decl(bi_1pin_with_name(SPI_CSN, "SPI CS"));
 
-    gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
-    gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, GPIO_OUT);
-    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, true);
-    bi_decl(bi_1pin_with_name(PICO_DEFAULT_SPI_CSN_PIN, "SPI CS"));
-#endif
+    spi_init(SPI_PORT, 2*1000*1000);
+    spi_set_format(SPI_PORT, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
 
-    spi_init(PICO_DEFAULT_SPI, 70000000);
-#if 0
-    spi_set_format(PICO_DEFAULT_SPI, 8, SPI_CPOL_1, SPI_CPHA_0, SPI_MSB_FIRST);
-#endif
-
-#if 0
     // reboot
     reg_write(0x3a, 0x5a);
     sleep_us(50);
@@ -85,7 +83,6 @@ void pmw3360_init() {
     reg_write(0x10, 0x00);
     pid = reg_read(0x00);
     rev = reg_read(0x01);
-#endif
 }
 
 int main() {
@@ -99,13 +96,11 @@ int main() {
     while(true) {
         uint64_t now = time_us_64();
         matrix_task(now);
-#if 1
         backlight_task(now);
         ledarray_task(now);
-#endif
         if ((now - last) > 1000000) {
             last = now;
-            printf("Hello Keyball61: pid=%02x rev=%02x sck=%d tx=%d rx=%d\n", pid, rev, PICO_DEFAULT_SPI_SCK_PIN, PICO_DEFAULT_SPI_TX_PIN, PICO_DEFAULT_SPI_RX_PIN);
+            printf("Hello Keyball61: pid=%02x rev=%02x rx=%d sck=%d tx=%d\n", pid, rev, SPI_RX, SPI_SCK, SPI_TX);
         }
     }
 }
